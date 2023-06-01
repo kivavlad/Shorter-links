9c39b7f8d8889d5161777d168605976dbf38264c
Влад <melnikovv717@gmail.com>
2023-06-01 14:15:13 +0300

    create motion animations

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,4 +1,5 @@
 import css from "./home.css";
+import { motion } from "framer-motion";
 import Hero from "../../assets/images/illustration-working.svg";
 import brand from "../../assets/images/icon-brand-recognition.svg";
 import details from "../../assets/images/icon-detailed-records.svg";
@@ -8,22 +9,62 @@ import Shortens from "../Shortens/Shortens";
 
 const Home = () => {
 
+    const textAnimation = {
+        hidden: {
+            x: -100,
+            opacity: 0,
+        },
+        visible: custom => ({
+            x: 0,
+            opacity: 1,
+            transition: { delay: custom * 0.2 },
+        }),
+    };
+
+    const textAnimationSecond = {
+        hidden: {
+            y: 100,
+            opacity: 0,
+        },
+        visible: custom => ({
+            y: 0,
+            opacity: 1,
+            transition: { delay: custom * 0.2 },
+        }),
+    };
+
+    const cardsAnimation = {
+        hidden: {
+            y: 100,
+            opacity: 0,
+        },
+        visible: custom => ({
+            y: 0,
+            opacity: 1,
+            transition: { delay: custom * 0.2 },
+        }),
+    }
+
     return(
         <>
-            <section className="up_section">
+            <motion.section 
+                initial="hidden"
+                whileInView="visible"
+                className="up_section"
+            >
                 <div className="container">
                     <div className="up_section_content">
                         <div className="up_section_text">
-                            <div className="up_section_title">More than just shorter links</div>
-                            <div className="up_section_subtitle">Build your brands recognition and get detailed insights on how your links are performing.</div>
-                            <button className="button up_section_button">Get Started</button>
+                            <motion.div className="up_section_title" variants={textAnimation}>More than just shorter links</motion.div>
+                            <motion.div className="up_section_subtitle" variants={textAnimation}>Build your brands recognition and get detailed insights on how your links are performing.</motion.div>
+                            <motion.button className="button up_section_button" variants={textAnimation}>Get Started</motion.button>
                         </div>
                         <div className="up_section_image">
                             <img src={Hero} alt="hero" className="up_section_hero_img" />
                         </div>
                     </div>
                 </div>
-            </section>
+            </motion.section>
 
             <section className="search">
             <div className="container">
@@ -35,53 +76,71 @@ const Home = () => {
                     </div>
             </section>
 
-            <section className="statistics">
+            <motion.section 
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ amount: 0.2 }}
+                className="statistics"
+            >
                    <div className="container">
                         <div className="result_container">
                             <Shortens />
                         </div>
-                        <div className="statictics_title">Advanced Statistics</div>
-                            <div className="statictics_subtitle">Track how your links are performing across the web with <br/>our advanced statistics dashboard.</div>
-                            <div className="statistics_cards">
+                        <motion.div className="statictics_title" custom={1} variants={textAnimationSecond}>Advanced Statistics</motion.div>
+                            <motion.div className="statictics_subtitle" custom={2} variants={textAnimationSecond}>Track how your links are performing across the web with <br/>our advanced statistics dashboard.</motion.div>
 
-                                <div className="statistics_card statistics_card_first">
+                            <motion.div 
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ amount: 0.5 }}
+                                className="statistics_cards"
+                            >
+
+                                <motion.div custom={1} variants={cardsAnimation} className="statistics_card statistics_card_first">
                                     <div className="statistics_card_circle">
                                         <img src={brand} alt="brand" className="statistics_card_img" />
                                     </div>
                                     <div className="statistics_card_title">Brand Recognition</div>
                                     <div className="statistics_card_subtitle">Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.</div>
-                                </div>
+                                </motion.div>
 
                                 <span className="statistics_card_line"></span>
 
-                                <div className="statistics_card statistics_card_second">
+                                <motion.div custom={2} variants={cardsAnimation} className="statistics_card statistics_card_second">
                                     <div className="statistics_card_circle">
                                         <img src={details} alt="brand" className="statistics_card_img" />
                                     </div>
                                     <div className="statistics_card_title">Detailed Records</div>
                                     <div className="statistics_card_subtitle">Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</div>
-                                </div>
+                                </motion.div>
 
                                 <span className="statistics_card_line"></span>
 
-                                <div className="statistics_card statistics_card_third">
+                                <motion.div custom={3} variants={cardsAnimation} className="statistics_card statistics_card_third">
                                     <div className="statistics_card_circle">
                                         <img src={fully} alt="brand" className="statistics_card_img" />
                                     </div>
                                     <div className="statistics_card_title">Fully Customizable</div>
                                     <div className="statistics_card_subtitle">Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</div>
-                                </div>
-                            </div>
+                                </motion.div>
+                            </motion.div>
                    </div>
-            </section>
-            <section className="down_section">
+            </motion.section>
+
+            <motion.section 
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ amount: 0.2 }}
+                className="down_section"
+                style={{overflow: 'hidden'}}
+            >
                 <div className="container">
                     <div className="down_section_content">
-                        <div className="down_section_title">Boost your links today</div>
-                        <button className="button down_section_button">Get Started</button>
+                        <motion.div className="down_section_title" custom={1} variants={textAnimationSecond}>Boost your links today</motion.div>
+                        <motion.button custom={1} variants={textAnimationSecond} className="button down_section_button">Get Started</motion.button>
                     </div>
                 </div>
-            </section>
+            </motion.section>
         </>
     )
 }
